fba9415a83ba5f45adea6f7656fc5eeac683e348
Banan320 <alex-gerber@mail.ru>
2024-02-12 17:52:42 +0300

    типы данных

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-let a = 1;
-console.log(a);
-a = "test";
-console.log(a);
+// let a = 1;
+// console.log(a);
+// a = "test";
+// console.log(a);
+
+// часовая ставка
+const payRateUSD = 80;
+const projectHours = 40;
+const availableHours = (11 - 2) * 5;
+console.log(`Смогу лия работать? ${availableHours > projectHours}`);
+console.log(`Стоимость работы: ${projectHours * payRateUSD}$`);
